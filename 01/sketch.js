07da9841ab1570ac431626c1e15ec78649a2bf49
mariabetan <31419333+mariabetan@users.noreply.github.com>
2017-08-28 14:44:45 -0500

    Add files via upload

---
+++ 01/sketch.js
@@ -0,0 +1,678 @@
+function setup() { 
+  createCanvas(400, 400);
+
+  background(215, 205,170);
+  
+  fill(233,222,203);
+  strokeWeight(2);
+  stroke(0,0,0);
+  triangle (85,10,170,400,0,400);
+  
+ noFill();
+  strokeWeight(1.5);
+  stroke(0,0,0);
+  triangle (0,55,160,160,0,95);
+  
+        fill(253, 238, 205, 100);
+  noStroke();
+  ellipse (150,110,55);
+  
+  
+  fill(255,208,44);
+  noStroke();
+  triangle (105,55,135,95,85,95);
+  
+  
+  noStroke();
+  fill(187, 209, 196,105);
+  triangle(0,275,100,400,2,400);
+  
+  
+  noFill();
+  strokeWeight(2);
+  stroke(0,0,0);
+  triangle (85,10,170,400,0,400);
+  
+  fill(143,155,151,155);
+  noStroke();
+  ellipse(40,45,35);
+  
+  noFill();
+  stroke(0,0,0);
+  strokeWeight(1.5);
+  ellipse (150,110,40);
+  
+    noFill();
+  stroke(255, 255, 255);
+  strokeWeight(2);
+  ellipse (150,110,33);
+  
+  fill(196, 202, 200);
+  noStroke();
+  ellipse (150,110,27);
+  
+    fill(82, 110, 149, 200);
+  noStroke();
+  ellipse (150,110,25);
+  
+    fill(77, 109, 156);
+  noStroke();
+  ellipse (150,110,15);
+  
+  fill(255, 255, 255, 100);
+  noStroke();
+  rect(200,0,5,120);
+  
+  stroke(0,0,0);
+  strokeWeight(1.5);
+  line(200,0,200, 120);
+    stroke(0,0,0);
+  strokeWeight(1.5);
+  line(205,0,205, 120);
+      stroke(0,0,0);
+  strokeWeight(1);
+  line(175,20,235, 20);
+    stroke(0,0,0);
+  strokeWeight(1);
+  line(175,23,235, 23);
+    stroke(0,0,0);
+  strokeWeight(1);
+  line(175,100,235, 100);
+   stroke(0,0,0);
+  strokeWeight(1);
+  line(175,102,235, 102);
+  stroke(0,0,0);
+  strokeWeight(1);
+  line(175,104,235, 104);
+  
+  noFill();
+  stroke(0,0,0);
+  strokeWeight(1);
+  ellipse (85,180,20);
+  
+    noFill();
+  stroke(0,0,0);
+  strokeWeight(0.5);
+  ellipse (205,50,30);
+  
+  fill(215, 205,170);
+  noStroke();
+  rect(212, 30, 40,40);
+  
+  
+  
+  point(85,180);
+  point(83,183);
+  point(88,176);
+  point(80,184);
+  point(80,172);
+  point(80,178);
+  point(80,168);
+  point(80,170);
+  point(80,188);
+  point(80,190);
+  point(80,187);
+  point(80,180);
+  point(80,182);
+  point(80,186);
+  point(80,174);
+  point(80,176);
+  point(80,191);
+  
+  point(82,184);
+  point(82,172);
+  point(82,178);
+  point(82,168);
+  point(82,170);
+  point(82,188);
+  point(82,190);
+  point(82,187);
+  point(82,180);
+  point(82,182);
+  point(82,186);
+  point(82,174);
+  point(82,176);
+  
+  
+	point(84,184);
+  point(84,172);
+  point(84,178);
+  point(84,168);
+  point(84,170);
+  point(84,188);
+  point(84,190);
+  point(84,187);
+  point(84,180);
+  point(84,182);
+  point(84,186);
+  point(84,174);
+  point(84,176);
+  point(84,192);
+
+  point(86,184);
+  point(86,172);
+  point(86,178);
+  point(86,168);
+  point(86,170);
+  point(86,188);
+  point(86,190);
+  point(86,187);
+  point(86,180);
+  point(86,182);
+  point(86,186);
+  point(86,174);
+  point(86,176);
+  point(86,192);
+ 
+ 
+  
+	point(88,184);
+  point(88,172);
+  point(88,178);
+  point(88,168);
+  point(88,170);
+  point(88,188);
+  point(88,190);
+  point(88,187);
+  point(88,180);
+  point(88,182);
+  point(88,186);
+  point(88,174);
+  point(88,176);
+  point(88,192);
+  
+  point(90,184);
+  point(90,172);
+  point(90,178);
+  point(90,168);
+  point(90,170);
+  point(90,188);
+  point(90,190);
+  point(90,187);
+  point(90,180);
+  point(90,182);
+  point(90,186);
+  point(90,174);
+  point(90,176);
+  
+  point(92,184);
+  point(92,172);
+  point(92,178);
+ 	point(92,170);
+  point(92,188);
+  point(92,190);
+  point(92,180);
+  point(92,182);
+  point(92,186);
+  point(92,174);
+  point(92,176);
+  
+  point(78,184);
+  point(78,172);
+  point(78,178);
+ 	point(78,170);
+  point(78,188);
+  point(78,190);
+  point(78,180);
+  point(78,182);
+  point(78,186);
+  point(78,174);
+  point(78,176);
+  
+  point(76,184);
+  point(76,172);
+  point(76,178);
+ 	point(76,170);
+  point(76,188);
+  point(76,180);
+  point(76,182);
+  point(76,186);
+  point(76,174);
+  point(76,176);
+  
+  
+  point(74,184);
+  point(74,172);
+  point(74,178);
+  point(74,180);
+  point(74,182);
+  point(74,186);
+  point(74,174);
+  point(74,176);
+  
+  point(94,184);
+  point(94,172);
+  point(94,178);
+ 	point(94,170);
+  point(94,188);
+  point(94,180);
+  point(94,182);
+  point(94,186);
+  point(94,174);
+  point(94,176);
+  
+  point(96,184);
+  point(96,172);
+  point(96,178);
+  point(96,180);
+  point(96,182);
+  point(96,186);
+  point(96,174);
+  point(96,176);
+  
+  point(72,184);
+  point(72,178);
+  point(72,180);
+  point(72,182);
+  point(72,174);
+  point(72,176);
+  
+ 
+  stroke(1);
+  line (0,245,100,205);
+  
+    strokeWeight(1.5);
+  line (0,248,100,208);
+  
+  
+  strokeWeight(3.5);
+  stroke(194, 134, 123);
+  line (0,265,145,195);
+  
+    strokeWeight(0.5);
+  stroke(0,0,0);
+  line (0,267,145,195);
+  
+      strokeWeight(2.5);
+  stroke(0,0,0);
+    line (0,288,80,200);
+  
+  stroke(154, 163, 120);
+        strokeWeight(3);
+  line (30,258,80,202);
+  
+  stroke(0,0,0);
+ strokeWeight(2.5);
+  line (80,200, 260, 288);
+  
+    	strokeWeight(4.5);
+  stroke(246, 219, 116);
+    line(99,278, 200,215);
+  
+    	strokeWeight(1.5);
+  stroke(0,0,0);
+    line(99,276, 200,213);
+  
+    stroke(154, 163, 120);
+        strokeWeight(3);
+  line (80,203,260,290);
+  
+      stroke(154, 163, 120);
+        strokeWeight(3);
+  line (80,203,260,290);
+  
+  stroke(0,0,0);
+  strokeWeight(1);
+  line(0,275,100,400);
+  
+  noStroke();
+  fill(160, 126, 65);
+  triangle(29, 385, 165, 380, 145, 290);
+  
+  
+  strokeWeight(3.5);
+  noFill()
+  stroke(178,118,78);
+  ellipse(200,327,65);
+  
+    strokeWeight(3.5);
+  noFill()
+  stroke(218,195,91);
+  ellipse(200,327,45);
+  
+      noStroke();
+  fill(0,0,0);
+  ellipse(200,327,20);
+  
+  noStroke();
+  fill(78, 115, 81);
+  quad(147, 289, 167, 272, 230, 380, 167, 380);
+  
+strokeWeight(5);
+  stroke(235, 185, 174);
+  noFill();
+  
+  noStroke();
+  fill(255,255,255);
+  arc(200,327,20,20,1.0472,4,2);
+
+		arc(71, 296, 50, 50, HALF_PI+QUARTER_PI, QUARTER_PI+HALF_PI+PI);
+strokeWeight(4);
+  stroke(184, 104, 95);
+  arc(101, 263, 40,40, HALF_PI+QUARTER_PI, QUARTER_PI+HALF_PI+PI);
+  
+  arc(70, 295, 50, 50, HALF_PI+QUARTER_PI, QUARTER_PI+HALF_PI+PI);
+  strokeWeight(1);
+  stroke(0,0,0);
+
+  noFill();
+  
+  strokeWeight(1);
+  stroke(0,0,0);
+  arc(103, 265, 40,40, HALF_PI+QUARTER_PI, QUARTER_PI+HALF_PI+PI);
+  
+  stroke(0,0,0);
+  arc(71, 296, 50, 50, HALF_PI+QUARTER_PI, QUARTER_PI+HALF_PI+PI);
+  
+  arc(125, 237,30,30, HALF_PI+QUARTER_PI, QUARTER_PI+HALF_PI+PI);
+  
+    strokeWeight(2);
+   stroke(147, 98, 81);
+  arc(124, 235,30,30, HALF_PI+QUARTER_PI, QUARTER_PI+HALF_PI+PI);
+
+  strokeWeight(2);
+   stroke(147, 98, 81);
+  arc(138, 218,15,15, HALF_PI+QUARTER_PI, QUARTER_PI+HALF_PI+PI);
+
+  strokeWeight(0.5);
+   stroke(0,0,0);
+  arc(138, 218,15,15, HALF_PI+QUARTER_PI, QUARTER_PI+HALF_PI+PI);
+  
+
+  fill(209, 143, 155);
+  noStroke();
+  rect(185, 155, 40, 40);
+  
+    stroke(0,0,0);
+  line(45,316, 195, 180);
+  
+    stroke(0,0,0);
+  line(68,268, 190,180);
+  
+  fill(97, 97, 125);
+  stroke(0,0,0);
+  ellipse(150,340,50,50);
+  
+    fill(227, 224, 209);
+  noStroke();
+  strokeWeight(1.5);
+  ellipse(150,340,35,35);
+  
+      fill(0,0,0);
+  noStroke();
+  strokeWeight(1.5);
+  ellipse(155,340,10,10);
+  
+  fill(255,0,0);
+	
+triangle(160, 335, 195, 265, 200, 275);
+  
+  fill(255,255, 255);
+	triangle(160, 335, 177, 300, 176, 310);
+
+  fill(0,0,0);
+  quad(195, 260, 210, 230, 220, 250, 202,275); 
+  
+  fill(255,10,10);
+  triangle(145, 340, 70, 310, 75, 320);
+  
+  stroke(255, 255, 255);
+  strokeWeight(2.5);
+  line(110, 330, 111, 328);
+  
+    stroke(255, 255, 255);
+  strokeWeight(1.5);
+  line(104, 327, 106, 324);
+  
+  
+      stroke(0,0,0);
+  strokeWeight(1.5);
+  line(102, 327, 105, 324);
+  
+      stroke(255, 255, 255);
+  strokeWeight(1.5);
+  line(101, 327, 104, 324);
+  
+
+  
+      stroke(0,0,0);
+  strokeWeight(1.5);
+  line(100, 327, 103, 324);
+  
+      stroke(255, 255, 255);
+  strokeWeight(1.5);
+  line(99, 327, 102, 324);
+  
+    
+      stroke(0,0,0);
+  strokeWeight(1.5);
+  line(98, 327, 101, 324);
+  
+      stroke(255, 255, 255);
+  strokeWeight(1.5);
+  line(97, 327, 100, 324);
+  
+  
+      stroke(255, 255, 255);
+  strokeWeight(3.5);
+  line(88, 322, 90, 317);
+  
+        stroke(0,0,0);
+  strokeWeight(1.5);
+  line(85, 322, 87, 317);
+  
+      stroke(255, 255, 255);
+  strokeWeight(1.5);
+  line(84, 322, 86, 317);
+  
+         stroke(0,0,0);
+  strokeWeight(1.5);
+  line(83, 322, 85, 317);
+  
+      stroke(255, 255, 255);
+  strokeWeight(1.5);
+  line(82, 319, 84, 315);
+           stroke(0,0,0);
+  
+
+  fill(255,0,0);
+  noStroke();
+  triangle(160,345,185,400,205,400);
+  
+  fill(255,255,255);
+  noStroke();
+
+  quad(170,355,201,395,183,400, 167,360);
+    
+  strokeWeight(1.5);
+  stroke(0,0,0);
+  line(176, 383, 189,380);
+  
+  
+  strokeWeight(2.5);
+  stroke(0,0,0);
+  line(178, 388, 193,384);
+  
+  strokeWeight(3.5);
+  stroke(0,0,0);
+  line(180, 395, 197,390);
+  
+
+  stroke(0,0,0);
+  strokeWeight(3);
+  fill(246, 226, 157);
+  rect(320, 30, 15, 15);
+  
+  fill(255,255,255,155);
+  strokeWeight(1);
+  quad(200,170,400,5, 400,20,210,180);
+	
+   fill(255,255,255,155);
+  strokeWeight(1);
+  quad(210,180,400,20, 400,40,220,190);
+	
+  
+     fill(255,255,255,155);
+  strokeWeight(1);
+  quad(220,190,400,40, 400,60,230,200);
+  
+  fill(255,255,255,155);
+  strokeWeight(1);
+  quad(230,200,400,60, 400,80,240,210);
+  
+  fill(255,255,255,155);
+  strokeWeight(1);
+  quad(220,225,400,80, 400,100,230,235);
+  
+  fill(255,255,255,155);
+  strokeWeight(1);
+  quad(230,235,400,100, 400,120,242,248);
+  
+    fill(255,255,255,155);
+  strokeWeight(1);
+  quad(242,248,400,120, 400,140,253,260);
+
+
+   fill(255,255,255,155);
+  strokeWeight(1);
+  quad(253,260,400,140, 400,160,266,272);
+
+   noFill();
+  strokeWeight(1);
+  quad(221,225,239,209,284,258,266,272);
+  
+     noFill();
+  strokeWeight(1);
+  quad(200,170,215,157,297,245,284,257);
+
+	     noFill();
+  strokeWeight(1);
+
+  quad(215,157,230,144,317,231,297,246);
+  
+  fill(215, 205,170);
+  noStroke();
+  quad(254,198,271,185,320,235,301,249);
+  
+  	     noFill();
+  stroke(0,0,0);
+  strokeWeight(1);
+  quad(230,144,243,134,323,225,317,230);
+
+    fill(215, 205,170);
+  noStroke();
+quad(279,142,400,40,400,60,285,140);
+  
+  fill(215, 205,170);
+  quad(279,142,400,40,400,58,302,140);
+  
+  fill(215, 205,170);
+  noStroke();
+quad(340,107,400,56,400,80,400,157);
+  
+  fill(0,0,0,155)
+  noStroke();
+  quad(262,154,294,128,319,128,271,167);
+  
+  fill(215, 205,170);
+noStroke();
+quad(280,177,400,80,400,110,295, 193);
+
+  fill(215, 205,170);
+  noStroke();
+quad(290,187,400,100,400,200,328,230,295,200);
+
+  fill(0,0,0)
+  noStroke();
+  quad(341,108,354,118,334,132, 324,123);
+  
+  fill(0,0,0)
+  noStroke();
+  quad(370,106,380,98,386,106, 377,112);
+  
+  
+  fill(0,0,0)
+  noStroke();
+  quad(380,116,390,108,398,116, 388,124);
+  
+  fill(0,0,0)
+  noStroke();
+  quad(361,72,374,62,388,74, 378,84);
+
+  fill(201,153,87);
+  strokeWeight(1);
+  triangle(379,97,280,177,291,188);
+  
+  stroke(0,0,0);
+  strokeWeight(1.5);
+  line(291,188,355,132);
+  
+  fill(0,0,0,200);
+  quad(335,150,355,132, 370, 150, 349, 167);
+  
+    fill(0,0,0);
+  quad(354,173,375,157, 395, 177, 375, 193);
+  
+  strokeWeight(4);
+  stroke(178,58,23);
+  line(210,400,360,220);
+  
+  strokeWeight(4);
+  stroke(0,0,0);
+  line(360,220, 400, 170);
+  
+  fill(0,0,0, 200)
+  noStroke();
+  quad(243,133,259,119,275,125,252,146);
+  
+  fill(0,0,0, 200)
+  noStroke();
+
+  quad(333,60,340,53,349,63,340,73);
+  
+  stroke(0,0,0);
+  strokeWeight(3.5);
+  line(250,320,400,350);
+  
+  strokeWeight(1.5);
+  line(265,270,400,390);
+  
+  fill(143,89,45,155);
+  noStroke();
+  triangle(282,256,297,245,400,372);
+  
+  fill(143,89,45,155);
+  noStroke();
+  triangle(316,230,324,225,400,332);
+  
+  fill(67,154,206,150);
+  noStroke();
+  ellipse(300,270,60,60);
+  
+   fill(186,110,78,200);
+  noStroke();
+  ellipse(380,145,45,45);
+  
+    fill(217,172,167,150);
+  noStroke();
+  ellipse(375,140,40,40);
+  
+   fill(221,203,141,150);
+  noStroke();
+  ellipse(365,90,45,45);
+  
+   fill(112,171,187,150);
+  noStroke();
+  ellipse(365,90,35,35);
+  
+     fill(186,103,61,150);
+  noStroke();
+  ellipse(235,140,55,55);
+  
+    fill(238,175,78,100);
+ stroke(0,0,0);
+  strokeWeight(1.5);
+  ellipse(225,150,60,60);
+  
+       
+}
